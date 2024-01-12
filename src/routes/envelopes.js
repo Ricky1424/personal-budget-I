549e3f0d9480e1fdf6d45944e40932acde4585f6
Ricky1424 <123240395+Ricky1424@users.noreply.github.com>
2024-01-12 15:09:55 +0000

    Finish coding
        - Created middleware to find index in array of given envelope name
        - Middleware converts param to lower case
        - Sends error and message if user searches with number
        - Send error and message if envelope does not exist
        - Created GET route to return specific envelope
        - Created PUT route to subtract funds from specific envelope
        - Created DELETE route to delete a specified envelope
        - Created another POST route that allows transfer from one fund to another.

--- src/routes/envelopes.js
+++ src/routes/envelopes.js
@@ -1,18 +1,43 @@
 // Require in express and extract the router object
 const { Router } = require('express');
 
-// Create an instance of the ROuter object and save to 'router'
+// Create an instance of the Router object and save to 'router'
 const router = Router();
 
 // Create an array to store the envelopes in
 const envelopes = [];
 let id = 0;
 
-// Middleware that runs with every request
-// router.use((req, res, next) => {
-//     console.log("MIDDLEWARE RUNNING SUCCESFULLY");
-//     next();
-// });
+
+// Middleware to find the index of an envelope given a name 
+router.param('envelopeName', (req, res, next, name) => {
+    const lowerCaseName = name.toLowerCase();
+    if (!isNaN(lowerCaseName)) {
+        res.status(400).send("Please enter an envelope name as a string")
+    }
+    const envelopeIndex = envelopes.findIndex(envelope => envelope.category === lowerCaseName);
+    if (envelopeIndex < 0) {
+        res.status(404).send("Envelope does not exist");
+    }
+    req.envelopeIndex = envelopeIndex;
+    next();
+})
+
+
+// Middleware to find the index of the toEnvelopeName
+router.param('toEnvelopeName', (req, res, next, toName) => {
+    const lowerCaseToName = toName;
+    if(!isNaN(lowerCaseToName)) {
+        res.status(400).send("Please enter destination envelope name as a string")
+    }
+    const toEnvelopeIndex = envelopes.findIndex(envelope => envelope.category === lowerCaseToName);
+    if (toEnvelopeIndex < 0) {
+        res.status(404).send("Destination envelope does not exist")
+    }
+    req.toEnvelopeIndex = toEnvelopeIndex;
+    next();
+})
+
 
 // Function to check validity of new envelope
 function newEnvelopeValidation(req, res, next) {
@@ -35,11 +60,13 @@ function newEnvelopeValidation(req, res, next) {
     };
 };
 
+
 // GET route that returns all envelopes
 router.get('/', (req, res, next) => {
-    console.log("Success");
+    console.log("Successfully retrieved all envelopes");
     res.status(200).send(envelopes)
 });
+
 
 // POST route to create new envelopes
 /*
@@ -58,7 +85,43 @@ router.post('/', newEnvelopeValidation, (req, res, next) => {
     res.status(201).send("Envelope created");
 });
 
+// GET route which return a single envelope by named request
+router.get('/:envelopeName', (req, res, next) => {
+    res.status(200).send(envelopes[req.envelopeIndex])
+});
+
+// PUT route which allows the subtracting of funds from an envelope
+router.put('/:envelopeName/:subtractAmount', (req, res, next) => {
+    const { subtractAmount } = req.params;
+    if (subtractAmount > envelopes[req.envelopeIndex].budgetAmount) {
+        res.status(400).send("Insufficient funds")
+    } else {
+        envelopes[req.envelopeIndex].budgetAmount -= subtractAmount;
+        res.status(200).send("Funds subtracted")
+    }
+});
+
+
+// DELETE route to remove an envelope
+router.delete('/:envelopeName', (req, res, next) => {
+    const { envelopeName } = req.params
+    envelopes.splice(req.envelopeIndex, 1);
+    res.status(204).send(`Succesfully deleted envelope: ${envelopeName}`);
+});
+
+
+// POST route to transfer funds from one envelope to another
+router.post('/:envelopeName/:toEnvelopeName/:transfer', (req, res, next) => {
+    const { transfer } = req.params;
+    if (isNaN(transfer)) {
+        res.status(400).send("Transfer amount must be a number")
+    }
+    envelopes[req.envelopeIndex].budgetAmount -= Number(transfer);
+    envelopes[req.toEnvelopeIndex].budgetAmount += Number(transfer);
+    console.log(`Succesfully transferred ${transfer} from ${envelopes[req.envelopeIndex].category} to ${envelopes[req.toEnvelopeIndex].category}`)
+    res.sendStatus(200);
+});
+
+
 // Export the routers
 module.exports = router;
-
-
